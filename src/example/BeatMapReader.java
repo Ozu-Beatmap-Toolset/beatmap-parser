92bb665ba9c0ccbbcfea7dcd3f534ad2e4b61b73
JohnWL <34081873+John-WL@users.noreply.github.com>
2021-10-01 23:57:50 -0400

    added an example for exporting beatmaps

--- src/example/BeatMapReader.java
+++ src/example/BeatMapReader.java
@@ -5,6 +5,7 @@ import osu.beatmap.hit_objects.circle.HitCircleData;
 import osu.beatmap.serialization.ParsedHitObjects;
 import osu.beatmap.serialization.BeatmapParser;
 import util.file.IOFile;
+import util.math.vector.Vector2Int;
 
 import java.io.File;
 import java.util.List;
@@ -21,7 +22,8 @@ public class BeatMapReader {
         // get the data object from the beatmap file
         Optional<Beatmap> map = BeatmapParser.decode(beatMapFile);
 
-        // do stuff with it!
+        // do stuff with it
+        // here, we just peak the first circle that we find after 120 pixels on the x axis
         map.ifPresent(beatMap -> {
             final ParsedHitObjects beatMapData = beatMap.hitObjects;
             final List<HitCircleData> circleDataList = beatMapData.hitCircleData;
@@ -29,7 +31,25 @@ public class BeatMapReader {
             circleDataList.stream()
                     .filter(hitCircleData -> hitCircleData.position.x > 120)
                     .findFirst()
-                    .ifPresent(hitCircleData -> System.out.println(hitCircleData.position.x));
+                    .ifPresent(hitCircleData -> System.out.println("Some arbitrary position of a circle: " + hitCircleData.position.x));
+        });
+
+        // you can also modify the data the beatmap contains
+        // here we just set the x position of the same circle we just peaked earlier to 0
+        map.ifPresent(beatMap -> {
+            final ParsedHitObjects beatMapData = beatMap.hitObjects;
+            final List<HitCircleData> circleDataList = beatMapData.hitCircleData;
+
+            circleDataList.stream()
+                    .filter(hitCircleData -> hitCircleData.position.x > 120)
+                    .findFirst()
+                    .ifPresent(hitCircleData -> hitCircleData.position.x = 0);
+        });
+
+        // you can also export the beatmap and play it in the game
+        map.ifPresent(beatmap -> {
+            final File destination = new File("src\\example\\some filename.osu");
+            BeatmapParser.encode(beatmap, destination);
         });
     }
 }
